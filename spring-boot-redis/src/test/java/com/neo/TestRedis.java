50a5d0ca70d8026946ba6557f4c5f89fd8ffd9bb
geyafeiCN <geyafei1226@163.com>
2019-05-05 16:56:53 +0800

    增加redisTemplate

--- spring-boot-redis/src/test/java/com/neo/TestRedis.java
+++ spring-boot-redis/src/test/java/com/neo/TestRedis.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.TimeUnit;
@@ -20,7 +19,7 @@ public class TestRedis {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
-    
+
 	@Autowired
     private RedisTemplate redisTemplate;
 
@@ -29,16 +28,17 @@ public class TestRedis {
         stringRedisTemplate.opsForValue().set("aaa", "111");
         Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
     }
-    
+
     @Test
     public void testObj() throws Exception {
         User user=new User("aa@126.com", "aa", "aa123456", "aa","123");
         ValueOperations<String, User> operations=redisTemplate.opsForValue();
         operations.set("com.neox", user);
+        operations.set("cc", user);
         operations.set("com.neo.f", user,1, TimeUnit.SECONDS);
         Thread.sleep(1000);
         //redisTemplate.delete("com.neo.f");
-        boolean exists=redisTemplate.hasKey("com.neo.f");
+        boolean exists=redisTemplate.hasKey("cc");
         if(exists){
         	System.out.println("exists is true");
         }else{
