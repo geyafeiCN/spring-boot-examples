8c6f8239259e6a5e3ea7aae2a2357cccb6cee5c4
geyafei <geyafei@ysstech.com>
2019-09-30 11:38:36 +0800

    补充一下

--- spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.neo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
-	
+
     @RequestMapping("/")
     public String index() {
+        System.out.println("--------测试---------");
         return "Hello Spring Boot 2.0!";
     }
 }
